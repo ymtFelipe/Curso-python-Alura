ea48d49081e9039bb517d464a1422ed043d834a6
Felipe Alcântara <felipe.alcantara.oliveira17@gmail.com>
2023-12-12 13:30:17 -0300

    enviando

--- orientação a objetos python 2/modelo.py
+++ orientação a objetos python 2/modelo.py
@@ -19,6 +19,8 @@ class Programa:
     def dar_like(self):
         self._likes += 1
 
+    def __str__(self):
+        return f'Nome: {self.nome} Likes: {self.likes}'
     
 
 
@@ -43,24 +45,61 @@ class Serie(Programa):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
 
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+        
+    def __len__(self):
+        return len(self._programas)
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    
+
+    @property
+    def listagem(self):
+        return self._programas
+    
+    
+    
     
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-
-
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
     print(programa)
 
 
-lista = [1, 2, 4, 5]
+print(f'Tá ou não tá? {demolidor in playlist_fim_de_semana.listagem}')
 
-print(repr(lista))
+print(type(playlist_fim_de_semana.listagem))
+
+# lista = [1, 2, 4, 5]
+# print(repr(lista))
